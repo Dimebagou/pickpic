435661c601b7d2d5522948bc877b11202fcefca5
Dimebagou <bersonvictor@gmail.com>
2024-04-18 04:37:25 +0200

    adding Clerk authentification system

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
     subsets: ["latin"],
@@ -20,10 +21,19 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={cn("font-poppins antialiased", poppins.className)}>
-                {children}
-            </body>
-        </html>
+        <ClerkProvider appearance={{
+            variables: { colorPrimary: '#624cf5'}
+        }}>
+            <html lang="en">
+                <body
+                    className={cn(
+                        "font-poppins antialiased",
+                        poppins.className
+                    )}
+                >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
