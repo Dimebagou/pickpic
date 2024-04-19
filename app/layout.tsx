7d22ceb835c7cf8d4d390fb22c55d87e3f09db9a
Dimebagou <bersonvictor@gmail.com>
2024-04-19 15:29:49 +0200

    fix eslint error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,33 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const poppins = Poppins({
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
-    variable: "--font-poppins"
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+    variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
-    title: "Pick Pic",
-    description: "AI-powered image generator"
+    title: 'Pick Pic',
+    description: 'AI-powered image generator',
 };
 
 export default function RootLayout({
-    children
+    children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <ClerkProvider appearance={{
-            variables: { colorPrimary: '#624cf5'}
-        }}>
+        <ClerkProvider
+            appearance={{
+                variables: { colorPrimary: '#624cf5' },
+            }}
+        >
             <html lang="en">
-                <body
-                    className={cn(
-                        "font-poppins antialiased",
-                        poppins.className
-                    )}
-                >
-                    {children}
-                </body>
+                <body className={cn('font-poppins antialiased', poppins.className)}>{children}</body>
             </html>
         </ClerkProvider>
     );
