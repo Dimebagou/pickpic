f66bf16c59fb1840fe54e1f6d8bf0ed3376953e5
Dimebagou <bersonvictor@gmail.com>
2024-04-19 11:53:45 +0200

    form and custom field setups

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,6 +4,6 @@ const Header = ({ title, subtitle }: { title: string; subtitle?: string }) => {
             <h2 className="h2-bold text-dark-600">{title}</h2>
             {subtitle && <p className="p-16-regular mt-4">{subtitle}</p>}
         </>
-    );
-};
-export default Header;
+    )
+}
+export default Header
